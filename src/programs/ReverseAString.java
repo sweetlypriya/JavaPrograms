9d98eca5307ceec8a763a4aea40d7908aea017d2
sweetlypriya <sweetlypriya9@gmail.com>
2025-12-17 15:06:57 +0530

    Reverse a String Program Added

---
+++ src/programs/ReverseAString.java
@@ -0,0 +1,18 @@
+package programs;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+		String a="Madam";
+		String rev="";
+		
+		for(int i=a.length()-1;i>=0;i--) {
+			rev=rev+a.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+	
+
+}
